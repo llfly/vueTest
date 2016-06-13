bfe980acfb181c85fa61c1aa6d488455bb01847e
llfly <lflylfy@gmail.com>
2016-06-13 19:01:19 +0800

    20160613

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,26 @@
 <template>
-	
+	<div class="header">
+		<div id="logo">稳定性检测系统</div>
+	</div>
 </template>
 <script>
-	
-
-	
+export default {
+  data () {
+    return {
+    }
+  }
+}
 </script>
-<style></style>
+<style scoped>
+.header{
+	width:100%;
+	height: 60px;
+	background-color: rgb(96,142,202);
+}
+#logo{
+	color: white;
+	line-height: 60px;
+	margin-left:50px;
+	font-weight: 600;
+}
+</style>
